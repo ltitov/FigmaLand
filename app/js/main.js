320b9b27ebd93abfba6bfc0e4e15440055d19eda
Artur i5 <itsitou88@gmail.com>
2021-04-09 16:59:54 +0300

    commit for continue work at home

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
-const iconMenu = document.querySelector('.menu__icon');
-const menuNav = document.querySelector('.menu__nav');
+const iconMenu = document.querySelector('.menu__icon'),
+  menuNav = document.querySelector('.menu__nav'),
+  playButton = document.querySelector('.features__play'),
+  videoContent = document.querySelector('.features');
 
 if (iconMenu) {
   iconMenu.addEventListener('click', function (e) {
@@ -8,3 +10,29 @@ if (iconMenu) {
     menuNav.classList.toggle('_active');
   });
 }
+
+// Play Video Content 
+
+
+
+videoContent.on('transitionEnd', function () {
+  let videos = document.querySelectorAll('.features__media video');
+  videos.forEach((el) => {
+    el.pause();
+    el.currentTime = 0;
+  });
+  playButton.forEach((el) => {
+    el.style.display = 'block';
+  });
+});
+
+playButton.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let video = e.currentTarget.closest('.features__media').querySelector('video');
+    video.play();
+    e.currentTarget.style.display = 'none';
+    setTimeout(() => {
+      video.volume = 0.5;
+    }, 1000);
+  });
+});
